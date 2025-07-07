fb4d686ef0c7602b8e01141c2129ea16f475ac47
muoivinh.github.io <vlxdmuoivinh@gmail.com>
2025-07-07 21:34:30 +0700

    1

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,16 +1,31 @@
-let products = [
+const products = [
     {
-        ms: 1,
-        category: 1,
-        size: 1,
-		detail: '',
-        image:'assets\img\gallery\gallery_14_3.jpg'
+        id: 'MV01-50.50-01.001',
+        category: 'Gạch ốp tường',
+        size: '500x500mm',
+        image: 'assets/product/MV01-500x500-01.001.jpg',
+
+        manufacturer: 'Prime',
+        idManufacturer:'',
+        distributor: 'Sơn quân'
     },
     {
-		ms: 2,
-        category: 1,
-        size: 1,
-		detail: '',
-        image:'assets\img\gallery\gallery_14_3.jpg'
+        id: 'MV01-50.50-01.001',
+        category: 'Gạch ốp tường',
+        size: '400x800cm',
+        image: 'assets/product/MV01-400x800-01.002.jpg',
+
+        manufacturer: 'Prime',
+        idManufacturer:'',
+        distributor: 'Sơn quân'
     },
+    
 ];
+
+
+const gallery = document.getElementById('gallery');
+
+products.forEach(val => {
+
+    gallery.insertAdjacentHTML('beforeend', "<div class='col-auto'> <div class='gallery-box '> <div class='gallery-img global-img'><img src='" + val.image + "'><span style='position: absolute; left: 5%; bottom: 5%; color: black;'>"+val.id+"</span><a href='" + val.image + "' class='icon-btn popup-image'><i class='fal fa-magnifying-glass-plus'></i></a></div></div></div>");
+});
