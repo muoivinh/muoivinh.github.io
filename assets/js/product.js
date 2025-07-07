9bf80ae0d317acb6cf6b4028786eb2ab765074f9
muoivinh.github.io <vlxdmuoivinh@gmail.com>
2025-07-07 18:19:47 +0700

    Init

---
+++ assets/js/product.js
@@ -0,0 +1,16 @@
+let products = [
+    {
+        ms: 1,
+        category: 1,
+        size: 1,
+		detail: '',
+        image:'assets\img\gallery\gallery_14_3.jpg'
+    },
+    {
+		ms: 2,
+        category: 1,
+        size: 1,
+		detail: '',
+        image:'assets\img\gallery\gallery_14_3.jpg'
+    },
+];
