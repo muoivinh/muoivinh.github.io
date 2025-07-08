29f7b4f4d4a73889c5d96c70b5f4a9a590383955
muoivinh.github.io <vlxdmuoivinh@gmail.com>
2025-07-08 21:25:42 +0700

    2025.07.08-1

--- assets/js/product.js
+++ assets/js/product.js
@@ -19,6 +19,38 @@ const products = [
         idManufacturer:'',
         distributor: 'Sơn quân'
     },
+        {
+        id: 'MV01-50.50-01.001',
+        category: 'Gạch ốp tường',
+        size: '500x500mm',
+        image: 'assets/product/MV01-500x500-01.001.jpg',
+
+        manufacturer: 'Prime',
+        idManufacturer:'',
+        distributor: 'Sơn quân'
+    },
+    {
+        id: 'MV01-50.50-01.001',
+        category: 'Gạch ốp tường',
+        size: '400x800cm',
+        image: 'assets/product/MV01-400x800-01.002.jpg',
+
+        manufacturer: 'Prime',
+        idManufacturer:'',
+        distributor: 'Sơn quân'
+    },
+        {
+        id: 'MV01-50.50-01.001',
+        category: 'Gạch ốp tường',
+        size: '400x800cm',
+        image: 'assets/product/MV01-400x800-01.002.jpg',
+
+        manufacturer: 'Prime',
+        idManufacturer:'',
+        distributor: 'Sơn quân'
+    }
+    
+   
     
 ];
 
@@ -27,5 +59,5 @@ const gallery = document.getElementById('gallery');
 
 products.forEach(val => {
 
-    gallery.insertAdjacentHTML('beforeend', "<div class='col-auto'> <div class='gallery-box '> <div class='gallery-img global-img'><img src='" + val.image + "'><span style='position: absolute; left: 5%; bottom: 5%; color: black;'>"+val.id+"</span><a href='" + val.image + "' class='icon-btn popup-image'><i class='fal fa-magnifying-glass-plus'></i></a></div></div></div>");
+    gallery.insertAdjacentHTML('beforeend', "<div class='col-auto'> <div class='gallery-box mv-border'> <div class='gallery-img global-img'><img src='" + val.image + "'><span style='position: absolute; left: 5%; bottom: 1%; color: black; font-size: 70%;'>"+val.id+"</span><a href='" + val.image + "' class='icon-btn popup-image'><i class='fal fa-magnifying-glass-plus'></i></a></div></div></div>");
 });
